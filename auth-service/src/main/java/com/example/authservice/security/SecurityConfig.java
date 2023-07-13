d31ce90301c915508cf7d83d0667f75a249ff2f0
leminhanhgch190017 <mle4635@gmail.com>
2023-07-13 17:26:13 +0700

    security

--- auth-service/src/main/java/com/example/authservice/security/SecurityConfig.java
+++ auth-service/src/main/java/com/example/authservice/security/SecurityConfig.java
@@ -1,14 +1,9 @@
 package com.example.authservice.security;
 
 import com.example.authservice.entity.Enum.Role;
-import com.example.authservice.entity.User;
-import com.example.authservice.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
